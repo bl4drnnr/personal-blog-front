71cfdba9f20042cdeb54e8679b0889fdeeb04534
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-03-09 12:27:47 +0100

    update post interface

--- src/services/post/get-post/get-post.interface.ts
+++ src/services/post/get-post/get-post.interface.ts
@@ -1,7 +1,41 @@
-export interface GetPostPayload {
-  slug: string;
+interface ILink {
+  name: string;
+  link: string;
+}
+
+interface IPicture {
+  type: 'picture';
+  width: string;
+  resource: string;
+}
+
+interface IList {
+  type: 'list-numeric' | 'list-bullet';
+  items: Array<any>;
+}
+
+interface ICode {
+  type: 'code';
+  lang: string;
+  content: string;
+}
+
+interface ITitle {
+  type: 'title' | 'subtitle' | 'subsubtitle';
+  content: string;
 }
 
 export interface GetPostResponse {
-  post: string;
+  title: string;
+  slug: string;
+  tags: string;
+  type: Array<string>;
+  description: string;
+  pageDescription: string;
+  intro: string;
+  timestamp: string;
+  footer?: string;
+  toc: object;
+  content: Array<string | IPicture | IList | ICode | ITitle>;
+  references: Array<ILink>;
 }
