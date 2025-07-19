cf33a552dda1ce87d12959fae18b548e89058ec4
bl4drnnr <mikhail.bahdashych@protonmail.com>
2025-07-19 21:32:33 +0200

    siteflow page fixed grid

--- src/app/pages/siteflow/siteflow.component.ts
+++ src/app/pages/siteflow/siteflow.component.ts
@@ -12,88 +12,74 @@ export class SiteflowComponent {
       title: 'Homepage',
       link: '/',
       icon: 'home-fill.svg',
-      iconAlt: 'Home',
-      description: 'LUCH Framework'
+      iconAlt: 'Home'
     },
     {
       title: 'Projects',
       link: '/projects',
       icon: 'image-ai-fill.svg',
-      iconAlt: 'Image AI',
-      isPrimary: true,
-      tag: 'New!',
-      description: 'A collection of AI-driven designs'
+      iconAlt: 'Image AI'
     },
     {
       title: 'Blog',
       link: '/blog',
       icon: 'article-fill.svg',
-      iconAlt: 'Article',
-      isSecondary: true,
-      description: 'Thoughts, insights and ideas on AI, design and creativity'
+      iconAlt: 'Article'
     },
     {
       title: 'Contact',
       link: '/contact',
       icon: 'contacts-fill.svg',
-      iconAlt: 'Contacts',
-      description: "Let's connect!"
+      iconAlt: 'Contacts'
     },
     {
       title: 'Subscribe',
       link: '/subscribe',
       icon: 'mail-add-fill.svg',
-      iconAlt: 'Mail Add',
-      description: 'Stay Updated'
-    },
-    {
-      title: '404 Page',
-      link: '/404',
-      icon: 'error-warning-fill.svg',
-      iconAlt: 'Error Warning',
-      isPrimaryShadow: true
-    },
-    {
-      title: 'Password',
-      link: '/401',
-      icon: 'lock-unlock-fill.svg',
-      iconAlt: 'Lock Unlock',
-      isPrimaryShadow: true
+      iconAlt: 'Mail Add'
     },
     {
       title: 'Style Guide',
       link: '/legal/style-guide',
       icon: 'settings-fill.svg',
-      iconAlt: 'Settings',
-      isPrimaryShadow: true
+      iconAlt: 'Settings'
     },
     {
       title: 'Licenses',
       link: '/legal/licenses',
       icon: 'file-info-fill.svg',
-      iconAlt: 'File Info',
-      isPrimaryShadow: true
+      iconAlt: 'File Info'
     },
     {
       title: 'Instruction',
       link: '/legal/instructions',
       icon: 'questionnaire-fill.svg',
-      iconAlt: 'Questionnaire',
-      isPrimaryShadow: true
+      iconAlt: 'Questionnaire'
     },
     {
       title: 'Changelog',
       link: '/legal/changelog',
       icon: 'sticky-note-fill.svg',
-      iconAlt: 'Sticky Note',
-      isPrimaryShadow: true
+      iconAlt: 'Sticky Note'
     },
     {
       title: 'Siteflow',
       link: '/siteflow',
       icon: 'layout-fill.svg',
-      iconAlt: 'Layout',
-      isPrimaryShadow: true
+      iconAlt: 'Layout'
     }
   ];
+
+  getFlowItemId(index: number): string | null {
+    switch (index) {
+      case 0:
+        return 'siteflow-homepage-tile';
+      case 7:
+        return 'siteflow-style-guide-tile';
+      case 11:
+        return 'siteflow-siteflow-tile';
+      default:
+        return null;
+    }
+  }
 }
