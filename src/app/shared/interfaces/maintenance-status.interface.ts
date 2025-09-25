d4bebc831165288c17f23e2d8265496db8cb66a7
bl4drnnr <mikhail.bahdashych@protonmail.com>
2025-09-25 19:00:52 +0200

    maintanence mode permanent

--- src/app/shared/interfaces/maintenance-status.interface.ts
+++ src/app/shared/interfaces/maintenance-status.interface.ts
@@ -1,10 +1,11 @@
 export interface MaintenanceStatus {
   isActive: boolean;
   message: string;
-  fromDate: Date;
-  toDate: Date;
+  fromDate?: Date;
+  toDate?: Date;
   heroImage: string;
   heroTitle: string;
   title: string;
   metaTitle: string;
+  isPermanent: boolean;
 }
